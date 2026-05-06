3e9c2bc8c1b4fdc97105c989c9c6093ffff26a97
CalvinDusHub <calvinmateus@gmail.com>
2026-05-06 18:29:11 -0300

    Cria estrutura inicial do tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class JogoDaVelha {
+  static void Main() {
+    String[] tabuleiro = new string[9];
+    int info = 1;
+
+    for (int i = 0; i < tabuleiro.Length; i++) {
+      tabuleiro[i] = info.ToString();
+      info++;
+    }
+
+    for (int i = 0; i < tabuleiro.Length; i++) {
+      Console.Write($"[{tabuleiro[i]}]");
+      if ((i + 1) % 3 == 0) {
+        Console.WriteLine();
+      }
+    }
+  }
+}
