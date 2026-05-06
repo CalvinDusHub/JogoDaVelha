215638cef65d22306553c62acef5b4858a157cfb
CalvinDusHub <calvinmateus@gmail.com>
2026-05-06 19:37:05 -0300

    Adiciona seleção de jogada e alternância de turnos

--- Program.cs
+++ Program.cs
@@ -2,8 +2,13 @@
 
 class JogoDaVelha {
   static void Main() {
-    String[] tabuleiro = new string[9];
+    //Estrutura tabuleiro
+    string[] tabuleiro = new string[9];
     int info = 1;
+    string xo = "X";
+
+    //Estrutura do jogo
+    int rounds = 0;
 
     for (int i = 0; i < tabuleiro.Length; i++) {
       tabuleiro[i] = info.ToString();
@@ -15,6 +20,36 @@ class JogoDaVelha {
       if ((i + 1) % 3 == 0) {
         Console.WriteLine();
       }
+    }
+
+    //Interação com o player
+    string jogada = Console.ReadLine();
+    Console.Clear();
+
+    while (rounds < 9) {
+      for (int i = 0; i < tabuleiro.Length; i++) {
+        if (tabuleiro[i] == jogada) {
+          tabuleiro[i] = xo;
+        }
+      }
+
+      for (int i = 0; i < tabuleiro.Length; i++) {
+        Console.Write($"[{tabuleiro[i]}]");
+        if ((i + 1) % 3 == 0) {
+          Console.WriteLine();
+        }
+      }
+
+      if (xo == "X") {
+        xo = "O";
+      }
+      else {
+        xo = "X";
+      }
+
+      jogada = Console.ReadLine();
+      rounds++;
+      Console.Clear();
     }
   }
 }
