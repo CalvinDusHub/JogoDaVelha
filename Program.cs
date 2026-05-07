45a5ab0d9761448b3259050b0d372952d0e8ead2
CalvinDusHub <calvinmateus@gmail.com>
2026-05-07 10:55:31 -0300

    Deixando mais bonito

--- Program.cs
+++ Program.cs
@@ -11,6 +11,10 @@ class JogoDaVelha {
     int rounds = 0;
     List<string> juiz = new List<string>();
 
+    Console.WriteLine("===================");
+    Console.WriteLine("   Jogo da Velha   ");
+    Console.WriteLine("===================");
+
     for (int i = 0; i < tabuleiro.Length; i++) {
       tabuleiro[i] = info.ToString();
       juiz.Add(info.ToString());
@@ -25,6 +29,7 @@ class JogoDaVelha {
     }
 
     //Interação com o player
+    Console.WriteLine($"Player {xo}: Selecione a posição no tabuleiro");
     string jogada = Console.ReadLine();
 
     //Validação
@@ -36,6 +41,9 @@ class JogoDaVelha {
     Console.Clear();
 
     while (rounds < 9) {
+      Console.WriteLine("===================");
+      Console.WriteLine("       Fight       ");
+      Console.WriteLine("===================");
       for (int i = 0; i < tabuleiro.Length; i++) {
         if (tabuleiro[i] == jogada) {
           tabuleiro[i] = xo;
@@ -54,43 +62,43 @@ class JogoDaVelha {
 
       //Horizontal
       if (tabuleiro[0] == tabuleiro[1] && tabuleiro[0] == tabuleiro[2]) {
-        Console.WriteLine($"{xo} ganhou");
+        Console.WriteLine($"Player {xo} ganhou");
         return;
       }
       else if (tabuleiro[3] == tabuleiro[4] && tabuleiro[3] == tabuleiro[5]) {
-        Console.WriteLine($"{xo} ganhou");
+        Console.WriteLine($"Player {xo} ganhou");
         return;
       }
       else if (tabuleiro[6] == tabuleiro[7] && tabuleiro[6] == tabuleiro[8]) {
-        Console.WriteLine($"{xo} ganhou");
+        Console.WriteLine($"Player {xo} ganhou");
         return;
       }
       //Vertical
       else if (tabuleiro[0] == tabuleiro[3] && tabuleiro[0] == tabuleiro[6]) {
-        Console.WriteLine($"{xo} ganhou");
+        Console.WriteLine($"Player {xo} ganhou");
         return;
       }
       else if (tabuleiro[1] == tabuleiro[4] && tabuleiro[1] == tabuleiro[7]) {
-        Console.WriteLine($"{xo} ganhou");
+        Console.WriteLine($"Player {xo} ganhou");
         return;
       }
       else if (tabuleiro[2] == tabuleiro[5] && tabuleiro[2] == tabuleiro[8]) {
-        Console.WriteLine($"{xo} ganhou");
+        Console.WriteLine($"Player {xo} ganhou");
         return;
       }
       //Diagonal
       else if (tabuleiro[0] == tabuleiro[4] && tabuleiro[0] == tabuleiro[8]) {
-        Console.WriteLine($"{xo} ganhou");
+        Console.WriteLine($"Player {xo} ganhou");
         return;
       }
       else if (tabuleiro[2] == tabuleiro[4] && tabuleiro[2] == tabuleiro[6]) {
-        Console.WriteLine($"{xo} ganhou");
+        Console.WriteLine($"Player {xo} ganhou");
         return;
       }
 
-      //Empate (velha)
+      //Empate
       if (juiz.Count == 0) {
-        Console.WriteLine("velha");
+        Console.WriteLine("Velha");
         return;
       }
 
@@ -103,6 +111,7 @@ class JogoDaVelha {
 
       rounds++;
 
+      Console.WriteLine($"Player {xo}: Selecione a posição no tabuleiro");
       jogada = Console.ReadLine();
 
       while (!juiz.Contains(jogada)) {
